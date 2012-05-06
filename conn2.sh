22426ca44b83c8fc0deb5799a09eefb5bd0d4d81
sebacjusz <sovietmudkipz22@gmail.com>
2012-05-06 12:26:14 +0200

    cleanup old/unnecessary files

--- conn2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#echo `id` >>/anon/tlog
-#echo `pwd` >>/anon/tlog
-chmod 666 /anon/rcp/tmp_rec.ogg
-#echo end >>/anon/tlog
-/anon/rcp/icecast-connect.py
--- restream.sh
+++
@@ -1,3 +0,0 @@
-killall -9 ezstream
-killall -9 ffmpeg
-ffmpeg -i http://pawlacz.tk:8000/stream.ogg -f mp3 -b 64k -ac 2 -y pipe1.mp3 & ezstream -c ezstream_reencode_mp3.xml &
