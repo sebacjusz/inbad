cce7654a87a522a60cc2d80662ed1917f85941bf
sebacjusz <sovietmudkipz22@gmail.com>
2013-02-09 21:17:00 +0100

    add jsonp endpoint for vichan

--- www.py
+++ www.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import cyclone.web
 import json
+import inba_cfg as conf
 
 
 class WebInterface(cyclone.web.Application):
@@ -9,7 +10,8 @@ class WebInterface(cyclone.web.Application):
         self.svc = svc
         handlers = [
                 (r'/loadbalancer.m3u', LBHandler),
-                (r'/listeners.jsonp', ListenersHandler) ]
+                (r'/listeners.jsonp', ListenersHandler),
+                (r'/vi_data.jsonp', ViHandler) ]
         settings = dict(debug=True)
         cyclone.web.Application.__init__(self,handlers,**settings)
 
@@ -25,3 +27,14 @@ class ListenersHandler(cyclone.web.RequestHandler):
         self.set_header("Content-Type", "text/javascript")
         d = json.dumps(self.application.svc.getListenerCount(t))
         return self.write('radioreply('+d+')')
+
+class ViHandler(cyclone.web.RequestHandler):
+    def get(self):
+        if not self.application.svc.meta:
+            return
+        d = {'listeners' : self.application.svc.getListenerCount('total'),
+                'listen_ogg' : self.application.svc.getListenURL('/vorbis-html5.ogg'),
+                'listen_mp3' : self.application.svc.getListenURL('/mp3-mq.mp3'),
+                'np' : {k:v for k,v in self.application.svc.meta.iteritems() if k in conf.safe_params}}
+        self.set_header("Content-Type", "text/javascript")
+        return self.write('radio_status_reply('+json.dumps(d)+')')
